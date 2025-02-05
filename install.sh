98f254fa8cc5a4addfae706f5241ab962850bc69
Sterling Abrahams <43232009+gunslingster@users.noreply.github.com>
2025-02-05 09:50:08 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+sudo apt install -y libsdl2-2.0-0
+sudo apt install -y libsdl2-dev
+sudo apt install -y libsdl2-image-dev
+sudo apt install -y make
+sudo apt-get install -y libsdl2-ttf-dev
